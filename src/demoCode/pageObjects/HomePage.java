d2664b75bc854c74c4b2839bfd18c90a4f03604d
kapilnegi67 <48856393+kapilnegi67@users.noreply.github.com>
2020-01-08 19:04:01 +0530

    Add files via upload

---
+++ src/demoCode/pageObjects/HomePage.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package demoCode.pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author kapilnegi
+ *
+ */
+public class HomePage {
+	WebDriver driver;
+
+    By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+
+    
+
+    public HomePage(WebDriver driver){
+
+        this.driver = driver;
+
+    }
+
+    //Get the User name from Home Page
+
+        public String getHomePageDashboardUserName(){
+
+         return    driver.findElement(homePageUserName).getText();
+
+        }
+
+}
